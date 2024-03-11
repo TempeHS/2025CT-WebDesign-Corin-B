5112e49cbe1d03500071e12bc77ab815e81843e4
Cornbarker91 <159206583+Cornbarker91@users.noreply.github.com>
2024-03-11 14:13:40 +1100

    ahhhhhhhhhhh

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let myintro = 15;
-let entername = 99.9999999;
+let entername = prompt("enter your name");
 
-if (myintro = entername) {
-    alert (myintro);
+let playerfeedback= "test";
+loadDIV();
+
+
+function loadDIV () {
+document.getElementById("ScriptThisDIV").innerHTML = "<H1>hello there " + entername + "</H1>"
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2> guess a number between 1 and 10" + "</H2>"
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerfeedback
 }
